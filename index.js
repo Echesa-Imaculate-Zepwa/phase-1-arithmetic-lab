30a222c87215e2474c70cb499a3745f9ea2989d0
imaculate-echesa <immaculatezepwa18@gmail.com>
2022-08-18 21:25:40 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+let random = 6;
+console.log (random);
+let num3 = 19;
+let num4 = 5;
+let mod = 19 % 5;
+let max = 20;
+ max = Math.max(16, 17, 18, 19, 20);
+ console.log(max);
